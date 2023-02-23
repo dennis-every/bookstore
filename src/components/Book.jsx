eeb69ea0ba1fad80e4fd02a76e4219a658d66cbc
Dennis Every <dennis@ansyra.com>
2023-02-23 17:59:23 -0400

    Add author and remove button to Book component

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,8 +1,15 @@
 /* eslint-disable react/prop-types */
 
 const Book = (props) => {
-  const { title } = props;
-  return <li>{title}</li>;
+  const { title, author } = props;
+  return (
+    <li>
+      {title}
+      {' - '}
+      {author}
+      <button type="button">Remove</button>
+    </li>
+  );
 };
 
 export default Book;
