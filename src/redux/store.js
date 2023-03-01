b90b6309a97c2d5315df48ed8566e4b87ed6abd0
Dennis Every <dennis@ansyra.com>
2023-03-01 12:15:03 -0400

    Add categoriesSlice with initial categories empty state, and checkStatus reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from '../features/booksSlice';
+import categoriesReducer from '../features/categoriesSlice';
 
 const store = configureStore({
   reducer: {
     books: booksReducer,
+    categories: categoriesReducer,
   },
 });
 
