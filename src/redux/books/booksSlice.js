9bb8b1ea6708dbdbf9aac0b458bd285a709908ce
Dennis Every <dennis@ansyra.com>
2023-03-02 22:01:23 -0400

    Refactor addBook logic to persist data on server

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -5,31 +5,43 @@ import axios from 'axios';
 const BOOKS_URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/NT4EbD5z0Cyfxmpz8y5k/books';
 
 const initialState = {
-  books: [],
-  status: 'idle', // 'idle' | 'loading' | 'succeeded' | 'failed'
+  books: {},
+  ifSucceed: false,
+  isLoading: false,
   error: null,
 };
 
-export const fetchBooks = createAsyncThunk(
+const fetchBooks = createAsyncThunk(
   'books/fetchBooks',
   async () => {
     try {
-      const response = await axios.get(BOOKS_URL);
-      return [...response.data];
+      const { data } = await axios.get(BOOKS_URL);
+      return data;
     } catch (err) {
       return err.message;
     }
   },
 );
 
+const addBook = createAsyncThunk('books/addBooks', async (book) => {
+  const data = JSON.stringify(book);
+  const headers = {
+    headers: {
+      'content-type': 'application/json',
+    },
+  };
+  try {
+    await axios.post(BOOKS_URL, data, headers);
+    return { message: 'Book added successfully' };
+  } catch (error) {
+    return error;
+  }
+});
+
 const booksSlice = createSlice({
   name: 'books',
   initialState,
   reducers: {
-    addBook: (state, action) => ({
-      ...state,
-      books: [...state.books, action.payload],
-    }),
     removeBook: (state, action) => ({
       ...state,
       books: state.books.filter((book) => book.item_id !== action.payload),
@@ -37,23 +49,33 @@ const booksSlice = createSlice({
   },
   extraReducers: {
     [fetchBooks.pending]: (state) => {
-      state.status = 'loading';
+      state.isLoading = true;
     },
     [fetchBooks.fulfilled]: (state, action) => {
-      state.status = 'succeeded';
+      state.isLoading = false;
+      state.ifSucceed = true;
       state.books = action.payload;
     },
     [fetchBooks.rejected]: (state) => {
-      state.status = 'failed';
-      state.books = [];
+      state.isLoading = false;
+      state.ifSucceed = false;
+    },
+
+    [addBook.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [addBook.fulfilled]: (state) => {
+      state.isLoading = false;
+      state.ifSucceed = true;
+    },
+    [addBook.rejected]: (state) => {
+      state.isLoading = false;
+      state.ifSucceed = false;
     },
   },
 });
 
-export const selectAllBooks = (state) => state.books.books;
-export const getBooksStatus = (state) => state.books.status;
-export const getBooksError = (state) => state.books.error;
-
-export const { addBook, removeBook } = booksSlice.actions;
+export { fetchBooks, addBook };
+export const { removeBook } = booksSlice.actions;
 
 export default booksSlice.reducer;
--- src/routes/Books.jsx
+++ src/routes/Books.jsx
@@ -2,39 +2,33 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Book from '../components/Book';
 import Form from '../components/Form';
-import {
-  selectAllBooks,
-  getBooksStatus,
-  getBooksError,
-  fetchBooks,
-} from '../redux/books/booksSlice';
+import { fetchBooks } from '../redux/books/booksSlice';
 
 const Books = () => {
   const dispatch = useDispatch();
-  const books = useSelector(selectAllBooks);
-  const booksStatus = useSelector(getBooksStatus);
-  const booksError = useSelector(getBooksError);
+  const ifSucceed = useSelector((store) => (store.books.ifSucceed));
+  const books = useSelector((store) => store.books.books);
+  const isLoading = useSelector((store) => store.books.isLoading);
 
   useEffect(() => {
-    if (booksStatus === 'idle') {
-      dispatch(fetchBooks());
-    }
-  }, [booksStatus, dispatch]);
+    dispatch(fetchBooks());
+  }, [ifSucceed, dispatch]);
 
   let content;
-  if (booksStatus === 'loading') {
+  if (isLoading) {
     content = <p>Loading...</p>;
-  } else if (booksStatus === 'succeeded') {
-    content = books.map((book) => (
-      <Book
-        key={book.item_id}
-        id={book.item_id}
-        title={book.title}
-        author={book.author}
-      />
-    ));
-  } else if (booksStatus === 'failed') {
-    content = <p>{booksError}</p>;
+  } else if (ifSucceed) {
+    content = Object.keys(books).map((key) => {
+      const currentBook = books[key][0];
+      return (
+        <Book
+          key={key}
+          id={key}
+          title={currentBook.title}
+          author={currentBook.author}
+        />
+      );
+    });
   }
 
   return (
